005e318a255bb17e7eb634db3c5b898980bb1dc3
AndriiMytiev <andrijmitev95@gmail.com>
2022-10-18 17:35:52 +0300

    add sections / add footer

--- fe-core-09/main.js
+++ fe-core-09/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
 
     $('.header-slider').bxSlider({
-    auto: true,
-    controls: true,
-    responsive :true,
-    speed: 700
+        auto: true,
+        controls: true,
+        responsive :true,
+        speed: 1000
+    });
+
+    $('.logo-slider').bxSlider({
+        auto: true,
+        controls: false,
+        responsive: true,
+        speed: 1000,
+        pager: false,
+        mimSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideWidth: 160
     });
 });
