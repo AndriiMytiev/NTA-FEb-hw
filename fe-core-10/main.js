dbc8f81c4165e733177b61ece175984da49a3391
AndriiMytiev <andrijmitev95@gmail.com>
2022-10-31 22:57:39 +0200

    add footer

---
+++ fe-core-10/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('body').on('click', '.tab-nav a', function(){
+        $('.tab-nav a').removeClass('active');
+        $(this).addClass('active');
+
+        let href = $(this).attr("href");
+        
+        $(".tab-pane").removeClass("active").removeClass("in");
+        let id = $(href).addClass("active");
+
+        setTimeout(function(){
+            $(href).addClass('in');
+        }, 200);
+
+        return false;
+    });
+
+    $('.reviews-slider').bxSlider({
+        auto: true,
+        controls: true,
+        responsive: true,
+        speed: 1000,
+        pager: false,
+        mimSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideWidth: 366
+    });
+});
